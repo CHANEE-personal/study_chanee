d94eb80bb3df6757a7b323681593656138219687
CHO CHAN HEE <cksgml4424@gmail.com>
2022-09-26 17:37:12 +0900

    static import 적용

--- src/main/java/com/study_chanee/config/SecurityConfig.java
+++ src/main/java/com/study_chanee/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.study_chanee.config;
 
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.*;
 
 @Configuration
 @EnableWebSecurity
@@ -24,6 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) {
         web.ignoring()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+                .requestMatchers(toStaticResources().atCommonLocations());
     }
 }
