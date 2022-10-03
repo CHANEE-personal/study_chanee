a5c7488a7cbb969f779a6aad7afd15676001d617
CHO CHAN HEE <cksgml4424@gmail.com>
2022-10-03 16:30:43 +0900

    회원가입 커밋

--- src/main/java/com/study_chanee/account/AccountController.java
+++ src/main/java/com/study_chanee/account/AccountController.java
@@ -1,9 +1,6 @@
 package com.study_chanee.account;
 
-import com.study_chanee.domain.Account;
 import lombok.RequiredArgsConstructor;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
